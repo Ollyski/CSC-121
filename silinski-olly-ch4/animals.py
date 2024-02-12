d778047ba8effc25ce1f8bdb7971fef918968776
Ollyski <146211302+Ollyski@users.noreply.github.com>
2024-02-11 22:22:37 -0500

    Add files via upload

---
+++ silinski-olly-ch4/animals.py
@@ -0,0 +1,5 @@
+animals = ['dog', 'cat', 'ferret']
+for animal in animals:
+  print(f"A {animal.lower()}, would make a great pet!")
+  print(f"A {animal.lower()} is legal to own in the state of North Carolina.\n")
+print("Any of these animals would make a great pet!")
