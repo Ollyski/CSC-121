6e71ad3b2e5ee3a92d50863e02d0f98bfbd3f845
Ollyski <146211302+Ollyski@users.noreply.github.com>
2024-02-18 20:14:27 -0500

    Add files via upload

---
+++ silinski-olly-ch5/alien_colors1.py
@@ -0,0 +1,6 @@
+alien_color = ['green', 'yellow', 'red']
+for alien in alien_color:
+  if alien == 'purple':
+    print("You earned 5 points!")
+  else:
+    print("Nice try!")
---
+++ silinski-olly-ch5/alien_colors2.py
@@ -0,0 +1,8 @@
+alien_color = ['green', 'yellow', 'red']
+for alien in alien_color:
+  if alien == 'green':
+    print("You earned 5 points!")
+  elif alien == 'yellow':
+    print("You earned 10 points!")
+  else:
+    print("You earned 10 points!")
