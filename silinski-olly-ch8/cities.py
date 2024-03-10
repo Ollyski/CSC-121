14fc76ab1391b7e8e2496c6fe93ca16eead1ab28
Ollyski <146211302+Ollyski@users.noreply.github.com>
2024-03-10 17:56:33 -0400

    Add files via upload

---
+++ silinski-olly-ch8/cities.py
@@ -0,0 +1,6 @@
+def describe_city(city, country='Default Country'):
+    print(f"{city} is in {country}.")
+
+describe_city('Reykjavik', 'Iceland')
+describe_city('New York', 'USA')
+describe_city('Tokyo') 
